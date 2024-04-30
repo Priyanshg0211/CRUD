6da42a019694983c5db725c454cc36c2c57d2bbf
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-30 23:17:17 +0530

    created employee page

---
+++ lib/pages/empolyee.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class employee extends StatefulWidget {
+  const employee({super.key});
+
+  @override
+  State<employee> createState() => _employeeState();
+}
+
+class _employeeState extends State<employee> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
